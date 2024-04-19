8e62f42a212ba15bdc7372d18a2265e687cf88d2
Jimena Jara <jimenaljara@gmail.com>
2024-04-19 19:00:54 +0000

    Update file exportandimport.js

--- server/utils/exportandimport.js
+++ server/utils/exportandimport.js
@@ -4,3 +4,6 @@
 
 //ejemplo para exportar una coleccion a la base de datos (modificar la ruta por la correspondiente)
 "mongoexport --collection roles --db web-db --out  x:\Users\User\Desktop\Proyecto Sustentable\roles.json"
+
+//admin@admin.com   pass admin123
+//editor@editor.com pass editor123
