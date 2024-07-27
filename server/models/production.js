535f359e44e49d98eaa0cccdf4a42d80aa2be82a
Bernardita Penayo <penayobernardita@gmail.com>
2024-07-27 20:33:45 +0000

    sitio prodeccion

---
+++ server/models/production.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const mongoosePaginate = require("mongoose-paginate");
+const site = require("./production");
+
+const ProductionSchema = mongoose.Schema({
+  date: Date,
+  creator_user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  state: String,
+  company:{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Company'
+  },
+  reviews:[{
+    date:Date,
+    reviewer_user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
+    },
+    comments:String,
+    state:String,
+  }],
+  production_volume:Number,
+  anual_average:Number,
+  active:Boolean
+});
+
+SiteSchema.plugin(mongoosePaginate);
+
+module.exports = mongoose.model("Production", ProductionSchema);
