52a4cf551c5d20f43ee4d2de7af54738eb6689a2
Bernardita Penayo <penayobernardita@gmail.com>
2024-07-27 20:41:48 +0000

    sitio energia

---
+++ server/router/energy.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const multiparty = require("connect-multiparty");
+const EnergyController = require("../controllers/energy");
+const md_auth = require("../middlewares/authenticated");
+
+const api = express.Energy();
+
+api.get("/energy/:id", [md_auth.asureAuth], EnergyController.getEnergy);
+api.get("/energies", [md_auth.asureAuth], EnergyController.getEnergies);
+api.post("/add-energy", [md_auth.asureAuth], EnergyController.createEnergy);
+api.put("/update-energy/:id",[md_auth.asureAuth], EnergyController.updateEnergy);
+api.delete("/delete-energy/:id", [md_auth.asureAuth], EnergyController.deleteEnergy);
+
+module.exports = api;
