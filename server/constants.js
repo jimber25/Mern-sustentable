0c7baf30384feae52684548ff6bd13474e0da01d
jimber25 <jimbersystem@gmail.com>
2025-01-14 17:12:39 -0300

    Update constants.js

--- server/constants.js
+++ server/constants.js
@@ -1,11 +1,11 @@
-const DB_USER = "localhost";
-const DB_PASSWORD = "123456";
-const DB_HOST = "web-db";
+const DB_USER =process.env.DB_USER|| "localhost";
+const DB_PASSWORD = process.env.DB_PASSWORD||"admin123";
+const DB_HOST = process.env.DB_HOST||"web-db";
 
-const API_VERSION = "v1";
-const IP_SERVER = "localhost";
+const API_VERSION = process.env.API_VERSION||"v1";
+const IP_SERVER =process.env.IP_SERVER|| "localhost";
 
-const JWT_SECRET_KEY = "gR7cH9Svfj8JLe4c186Ghs48hheb3902nh5DsA";
+const JWT_SECRET_KEY =process.env.JWT_SECRET_KEY|| "gR7cH9Svfj8JLe4c186Ghs48hheb3902nh5DsA";
 
 module.exports = {
     DB_HOST,
